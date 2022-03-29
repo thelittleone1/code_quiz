e2d262e3d5dbf52a39273555f97b3e4d1c4e451f
Urooj Qureshi <qurooj0226@gmail.com>
2022-03-29 11:46:20 -0700

    Updates to script2

--- script2.js
+++ script2.js
@@ -36,6 +36,7 @@ let questions = [{
             correctAnswer: "d"},
 ];
 
+// Creating Elements for DOM manipulation 
 var quizIntroEl = document.getElementById("quizIntro");
 var startButton = document.getElementById("startButton");
 
@@ -63,16 +64,18 @@ var endGame = document.getElementById("endGame");
 var playAgain = document.getElementById("playAgain");
 var clearHighScore = document.getElementById("clearHighScore");
 
+//Global Variables
 var finalQuestionIndex = questions.length;
-var initialquestionIndiex = 0;
+var initialquestionIndex = 0;
 var timeleft = 61;
 var timerInterval;
 var score = 0;
 var correctAnswerEl;
-var currentQuestion = questions[initialquestionIndiex];
+var currentQuestion = questions[initialquestionIndex];
 
+// Function to generate the quizQuestions
 function quizQuestions() {
-    if (initialquestionIndiex === finalQuestionIndex) {
+    if (initialquestionIndex === finalQuestionIndex) {
         return showScore();
     }
     currentQuestion;
@@ -83,6 +86,7 @@ function quizQuestions() {
     buttonD.innerHTML = currentQuestion.choiceD;
 }
 
+// Function to start the quiz
 function startQuiz() {
     quizIntroEl.classList.add("hide");
     startButton.classList.add("hide");
@@ -101,30 +105,106 @@ function startQuiz() {
     // quizIntroEl.style.display = "block";
 }
 
+// Function to display the score at the end of the quiz
 function showScore() {
     quizIntroEl.classList.add("hide");
     gameOver.style.display = "flex";
     scoreListEl.classList.remove("hide");
     clearInterval(timerInterval);
     highScoreInitials.value = "";
-    scoreListEl.innerHTML = "Your score is: " + timeleft;
+    scoreListEl.innerHTML = "Your score is: " + score + " of " + questions.length;
 }
 
+// this hands the submit button for the users in put of their score and name
+// also handles the storage of that content in localStorage
+submitButton.addEventListener("click", function highScore(){
+    if(highScoreInitials.value === "") {
+        alert("Initials cannot be blank");
+        return false;
+    } else {
+        var saveHighScore = JSON.parse(localStorage.getItem("saveHighScore")) || [];
+        var currentUser = highScoreInitials.value.trim();
+        var userHighScore = {
+            name: currentUser,
+            score: timeleft
+        };
+        // gameOver.style.display = "none";
+
+        highscoreContainer.classList.remove("hide")
+        highscoreContainer.style.display = "flex";
+        highScorePage.style.display = "block";
+
+        endGame.classList.remove("hide");
+        endGame.style.display = "flex";
+
+        saveHighScore.push(userHighScore);
+        localStorage.setItem("saveHighScore", JSON.stringify(saveHighScore));
+
+        generateScores();
+    }
+})
+
+// This displays the highscores stored in LocalStorage
+function generateScores() {
+    highScoreInitials.innerHTML = "";
+    highScoreScore.innerHTML = "";
+    var displayHighScores = JSON.parse(localStorage.getItem("saveHighScore")) || [];
+
+    for ( var i = 0; i < displayHighScores.length; i++ ) {
+        var enteredUserName = document.createElement("li");
+        var enteredUserScore = document.createElement("li");
+        enteredUserName.textContent = displayHighScores[i].name;
+        enteredUserScore.textContent = displayHighScores[i].score;
+        highScoreInitials.appendChild(enteredUserName);
+        highScoreScore.appendChild(enteredUserScore);
+    }
+}
+
+// Displays the high score play while hiding the the start page
+function scoreDisplay() {
+    // startQuiz.classList.remove("hide");
+    // gameOver.classList.remove("hide");
+    highScoresContainer.style.display = "flex";
+    highScorePage.style.display = "block";
+    endGame.style.display = "flex";
+    generateScores();
+}
+
+// Clears the high scores from localStorage
+function clearScore() {
+    window.localStorage.clear();
+    highScoreInitials.textContent = "";
+    highScoreScore.textContent = "";
+}
+
+// Function to allow you to replay the quiz
+function replayQuiz() {
+    // highScoresContainer.style.display = "none";
+    //gameOver.style.direction = "none";
+    startQuiz.classList.remove("hide");
+    startQuiz.style.display = "flex";
+    timeleft = 61;
+    score = 0;
+    initialquestionIndex = 0;
+}
+
+// This function checks that user selects the wrong or right answer
 function correctAnswer(answer) {
     correctAnswerEl = currentQuestion.correctAnswer;
 
-    if (answer === correctAnswerEl && initialquestionIndiex !== finalQuestionIndex) {
+    if (answer === correctAnswerEl && initialquestionIndex !== finalQuestionIndex) {
         alert("Correct!");
-        initialquestionIndiex++;
+        initialquestionIndex++;
         quizQuestions();
-    } else if (answer !== correctAnswerEl && initialquestionIndiex !== finalQuestionIndex) {
+    } else if (answer !== correctAnswerEl && initialquestionIndex !== finalQuestionIndex) {
         alert("Wrong!")
         timeleft = timeleft - 10;
-        initialquestionIndiex++;
+        initialquestionIndex++;
         quizQuestions();
     } else {
         showScore();
     }
 }
 
+// Start button for the quiz
 startButton.addEventListener("click",startQuiz);
